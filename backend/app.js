fd86ec5ad98f526536575f7c9269a59d96a110c6
mayuripy <115979827+mayuripy@users.noreply.github.com>
2023-09-15 11:31:22 +0530

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,12 @@
+// const express = require("express");
+
+// const app = express();
+
+// app.use("/",(req,res,next) =>{
+//     res.send("<h1>hellow</h1>");
+// })
+
+// app.listen(5000, () =>{
+//     console.log("connected to localhost port ${5000}");
+// })
+console.log("hello #");
