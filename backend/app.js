34702774f96ab308df226e6eae4ca03c97dee0ae
mayuripy <115979827+mayuripy@users.noreply.github.com>
2023-09-15 15:33:17 +0530

    ..

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,11 @@
-// const express = require("express");
+ import express from "express";
 
-// const app = express();
+ const app = express();
 
-// app.use("/",(req,res,next) =>{
-//     res.send("<h1>hellow</h1>");
-// })
+ app.use("/",(req,res,next) =>{
+    res.send("<h1>hellow</h1>");
+ })
 
-// app.listen(5000, () =>{
-//     console.log("connected to localhost port ${5000}");
-// })
-console.log("hello #");
+ app.listen(5000, () =>{
+    console.log("connected to localhost port ${5000}");
+ })
